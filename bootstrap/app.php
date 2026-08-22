16e158646fdb4bf1415d4eb0b6e0ffd48929b0a0
AwaaanZzz <whnofficial@gmail.com>
2026-08-22 21:42:55 +0700

    app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,8 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'log.activity' => \App\Http\Middleware\LogActivity::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
